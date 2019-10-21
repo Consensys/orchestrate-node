cb9c55a149ff068373cadcd944859f8c8cbacbe3
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-10-21 16:35:46 +0200

    chore(package.json): update installation config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
-
+const webpack = require("webpack");
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-    entry: [
-      './src/index.js'
-    ],
+    entry: {
+      library: './src/index.js',
+      contractregistry: './cli/contract-registry/index.js'
+    },
     watchOptions: {
       ignored: /node_modules/
     },
@@ -13,11 +14,11 @@ module.exports = {
     devtool: 'source-map',
     externals: [nodeExternals()],
     output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
-        publicPath: 'build/',
-        libraryTarget:'umd',
-        libraryExport: 'default'
+      path: path.join(__dirname, "build"),
+      filename: "[name].bundled.js",
+      library: "",
+      libraryTarget: "umd",
+      libraryExport: 'default'
     },
     module: {
         rules: [
@@ -26,9 +27,14 @@ module.exports = {
               exclude: /node_modules/,
               use: [
                 {loader: 'babel-loader'},
-                {loader: 'eslint-loader'}
+                {loader: 'eslint-loader'},
+                {loader: 'shebang-loader'}
               ]
             }
         ]
-    }
+    },
+    plugins: [
+      // Put the shebang back on.
+      new webpack.BannerPlugin({ banner: "#!/usr/bin/env node\n", raw: true }),
+    ]
 }
