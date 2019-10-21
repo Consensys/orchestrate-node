3494e2c8cc309489a8790ebc0539480a61170911
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-10-21 20:30:08 +0200

    fix@cli(contract-registry): remove babel registration

--- cli/contract-registry/index.js
+++ cli/contract-registry/index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-require('@babel/register')({
-    presets: ["@babel/preset-env"],
-    plugins: [
-        "@babel/plugin-syntax-dynamic-import",
-        "@babel/plugin-syntax-import-meta",
-        "@babel/plugin-proposal-class-properties",
-        "@babel/plugin-proposal-json-strings",
-        "@babel/plugin-transform-runtime"
-    ]
-})
+// require('@babel/register')({
+//     presets: ["@babel/preset-env"],
+//     plugins: [
+//         "@babel/plugin-syntax-dynamic-import",
+//         "@babel/plugin-syntax-import-meta",
+//         "@babel/plugin-proposal-class-properties",
+//         "@babel/plugin-proposal-json-strings",
+//         "@babel/plugin-transform-runtime"
+//     ]
+// })
 
 require('./cli').cli(process.argv);
