a4a9853fe5fa2dceffb9e8cc8d667b6876b5df14
Dario Anongba Varela <dario.varela@consensys.net>
2020-01-20 18:18:10 +0100

    topic by default

--- examples/produce-and-consume/app.ts
+++ examples/produce-and-consume/app.ts
@@ -1,5 +1,5 @@
 import { Consumer, Producer, ResponseMessage } from '../../src'
-import { EventType } from '../../src/types'
+import { EventType } from '../../src/definitions'
 
 const consume = async (consumer: Consumer) => {
   await consumer.connect()
