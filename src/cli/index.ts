d0fb434e6ad7681eeffafe262187aadd7c6e6240
Dario Anongba Varela <dario.varela@consensys.net>
2020-02-25 19:05:34 +0100

    fix commander

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -7,6 +7,6 @@ const program = new Command()
 program
   .version('2.0.0')
   .description('Orchestrate CLI')
-  .command('contracts', 'Contract Registry management')
-  .command('accounts', 'Accounts management')
+  .command('contracts', 'Contract Registry management', { executableFile: 'orchestrate-contracts' })
+  .command('accounts', 'Accounts management', { executableFile: 'orchestrate-accounts' })
   .parse(process.argv)
