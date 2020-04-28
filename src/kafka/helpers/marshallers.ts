9ed11c306b5b700b661ab5c794211c11a10e2b33
christian.tran <christian.trn@gmail.com>
2020-04-28 19:33:06 +0200

    fix(kafka): add kafka partition key in the producer

--- src/kafka/helpers/marshallers.ts
+++ src/kafka/helpers/marshallers.ts
@@ -1,3 +1,4 @@
+import * as crypto from 'crypto'
 import { Message } from 'kafkajs'
 
 import { tx } from '../../stubs'
@@ -19,7 +20,8 @@ export function marshalTransactionRequest(request: ITransactionRequest) {
     methodSignature: request.methodSignature,
     args: solFormatters.formatMethodArgs(request.methodSignature, request.args),
     privateFor: request.privateFor,
-    privateFrom: request.privateFrom
+    privateFrom: request.privateFrom,
+    privacyGroupId: request.privacyGroupId
   }
 
   const envelopeMessage = marshalTxRequest(request.id, request.chainName, request.authToken)
@@ -54,7 +56,34 @@ function marshalEnvelope(envelopeMessage: tx.ITxRequest): Message {
   const { encode } = tx.TxRequest
 
   // The type is Buffer on Node
-  return { value: encode(envelopeMessage).finish() as Buffer }
+  return { value: encode(envelopeMessage).finish() as Buffer, key: marshalKafkaKey(envelopeMessage) }
+}
+
+// Attribute kafka partition keys to messages to well attribute nonce
+// For a classic eth_sendRawTransaction transaction - <from>@<chain>
+// For a eea_sendRawTransaction with a privacyGroupID - <from>@orion-<privacyGroupID>@<chain>
+// For a eea_sendRawTransaction with a privateFor - <from>@orion-<hash(privateFor-privateFrom)>@<chain>
+export function marshalKafkaKey(envelopeMessage: tx.ITxRequest) {
+  if (envelopeMessage.params === undefined || envelopeMessage.params === null) {
+    return undefined
+  }
+  switch (envelopeMessage.method) {
+    case tx.Method.EEA_SENDPRIVATETRANSACTION: {
+      if (envelopeMessage.params.privacyGroupId !== undefined) {
+        return `${envelopeMessage.params.from}@orion-${envelopeMessage.params.privacyGroupId}@${envelopeMessage.chain}`
+      } else if (envelopeMessage.params.privateFor?.length! > 0) {
+        const raw: string[] = [...envelopeMessage.params.privateFor!, envelopeMessage.params.privateFrom!]
+        const key = crypto
+          .createHash('md5')
+          .update(raw.sort().join('-'))
+          .digest('hex')
+        return `${envelopeMessage.params.from}@orion-${key}@${envelopeMessage.chain}`
+      }
+      return `${envelopeMessage.params.from}@orion-${envelopeMessage.chain}`
+    }
+    default:
+      return `${envelopeMessage.params.from}@${envelopeMessage.chain}`
+  }
 }
 
 function marshalTxRequest(id?: string, chainName?: string, authToken?: string) {
