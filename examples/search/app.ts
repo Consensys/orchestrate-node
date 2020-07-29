2accee6ef66dd1cabff92e2db3f5c33dcf989776
Dario Anongba Varela <dario.varela@consensys.net>
2020-07-29 18:10:13 +0200

    example link

--- examples/search/app.ts
+++ examples/search/app.ts
@@ -6,7 +6,7 @@ import { TransactionClient } from '../../src'
 
 export const start = async () => {
   try {
-    const txClient = new TransactionClient('http://localhost:8041')
+    const txClient = new TransactionClient('http://localhost:8031')
 
     // Note: next statement is an example of how to get a transaction by UUID
     // const res = await txClient.get('87a5722a-0cfb-4cc9-a382-013965b36cb2')
