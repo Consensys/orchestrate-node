a4842fab6b64fac450700ed9ae73769c6304d532
Dario Anongba Varela <dario.varela@consensys.net>
2020-07-31 12:25:17 +0200

    fixes

--- examples/register-contract/app.ts
+++ examples/register-contract/app.ts
@@ -7,11 +7,15 @@ export const start = async () => {
   try {
     const contractRegistry = new ContractRegistry('localhost:8020')
 
+    const authToken =
+      'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiaHR0cHM6Ly9hdXRoMC5jb20vYXBpL3YyLyJdLCJleHAiOjE1OTYxMjQwNDcsImh0dHA6Ly9vcmNoZXN0cmF0ZS5pbmZvIjp7InRlbmFudF9pZCI6ImRhcmlvIn0sImlhdCI6MTU5NjEyMDQ0NywiaXNzIjoiT3JjaGVzdHJhdGUiLCJqdGkiOiJhYWI5Yjc0OC02MmQwLTQ1ZDgtODNjNC04OWMzZDczOTM0NzUiLCJuYmYiOjE1OTYxMjA0NDcsInNjcCI6WyJyZWFkOnVzZXJzIiwidXBkYXRlOnVzZXJzIiwiY3JlYXRlOnVzZXJzIl0sInN1YiI6ImUyZS10ZXN0In0.eftpqgmy5445E6vfpvqa0lxMfUK_-5GedMDoM0V4PsdmeVYo1QdtK9WYOHt-xCYmCdKWWKdecTHYk-kRHWBu-d4D45c3Q9xaS9D56SCrER6hGDoOKB_QfvHOxj5ut0qUlLryQAhlQ-wXwXbjVOozFym1qn6oGR9zDYnr9q2hUTpnYqJ2Wh7nxHZRJ-nbBSbUUEs2PPGejNpui4-QGH-2wKH9BQICUNzeNbCYRGjfC6Ix_HlVoBpii3rkaSIHlpOPgrpno98aOMFyzBxhV1e_J4j4JOsHaVzsBLXf_uZWC4MUFvjpwmo-Gvxu-5sfgliMHydYzNlQIIxqoGEIwR03sQ'
+
     await contractRegistry.register({
       name: 'SimpleToken',
       abi: SimpleToken.abi,
       bytecode: SimpleToken.bytecode,
-      deployedBytecode: SimpleToken.deployedBytecode
+      deployedBytecode: SimpleToken.deployedBytecode,
+      authToken
     })
 
     console.log(await contractRegistry.getCatalog())
