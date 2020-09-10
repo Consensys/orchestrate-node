f96b2c8a21b2342955456f36498b3c8fab34e3ac
christian.tran <christian.trn@gmail.com>
2020-09-10 16:01:52 +0200

    feat(retry policy): add interface and example

---
+++ examples/gasprice-retry/index.ts
@@ -0,0 +1,5 @@
+// tslint:disable: no-console
+
+import { start } from './app'
+
+start().catch(console.log)
--- src/http/types/IGasPricePolicy.ts
+++ src/http/types/IGasPricePolicy.ts
@@ -2,4 +2,11 @@ import { Priority } from './Priority'
 
 export interface IGasPricePolicy {
   priority?: Priority
+  retryPolicy?: IRetryPolicy
+}
+
+export interface IRetryPolicy {
+  increment: number
+  limit: number
+  interval: string
 }
