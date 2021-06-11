109b061da84921a02de22e2c00c1c6dfdd512cbf
ggarri <gabriel.garrido@consensys.net>
2021-06-11 15:52:56 +0200

    using hex for signing and verifying data

--- examples/sign-verify/app.ts
+++ examples/sign-verify/app.ts
@@ -2,6 +2,15 @@
 
 import { OrchestrateClient } from '../../src'
 
+function convertToHex(str: string): string {
+  let hex = ''
+  for (let i = 0; i < str.length; i++) {
+    hex += '' + str.charCodeAt(i).toString(16)
+  }
+
+  return '0x' + hex
+}
+
 export const start = async () => {
   try {
     const client = new OrchestrateClient('http://localhost:8031')
@@ -9,7 +18,7 @@ export const start = async () => {
     const privateKey = 'f368b15ea01bafd199d4d39daee2b1d281bf78193c590ef79c233016b3b43f81'
     const account = await client.importAccount({ privateKey })
 
-    const data = 'my data to sign'
+    const data = convertToHex('my data to sign')
     const signature = await client.sign(account.address, data)
 
     await client.verifySignature({
